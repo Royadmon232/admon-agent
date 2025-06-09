1b35a15d917276c4c6e3fff36f833f78fe51baaf
Royadmon232 <royadmon23@gmail.com>
2025-06-09 23:40:05 +0300

    fix: update imports to use ragChain.js instead of smartAnswer.js

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -1,6 +1,6 @@
 import { normalize } from '../utils/normalize.js';
 import { createRetrievalChain } from 'langchain/chains/retrieval';
-import { smartAnswer, semanticLookup, lookupRelevantQAs } from './smartAnswer.js';
+import { smartAnswer } from './ragChain.js';
 import { checkDuplicateMessage } from '../utils/deduplication.js';
 import { intentDetect, buildSalesResponse } from './salesTemplates.js';
 import { sendWhatsApp } from './twilioService.js';
